bb667f95621c06ccfde1000c6df18d33f559b3e3
kylan120 <142834131+kylan120@users.noreply.github.com>
2023-11-29 14:06:22 -0600

    Here's the project

---
+++ Source/OneVOneMe/OneVOneMeCharacter.h
@@ -0,0 +1,115 @@
+// OneVOneMeCharacter.h
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "Logging/LogMacros.h"
+#include "OneVOneMeCharacter.generated.h"
+
+class UInputComponent;
+class USkeletalMeshComponent;
+class UCameraComponent;
+class UInputAction;
+class UInputMappingContext;
+struct FInputActionValue;
+
+DECLARE_LOG_CATEGORY_EXTERN(LogTemplateCharacter, Log, All);
+
+UCLASS(config=Game)
+class AOneVOneMeCharacter : public ACharacter
+{
+	GENERATED_BODY()
+
+	/** Pawn mesh: 1st person view (arms; seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category=Mesh)
+	USkeletalMeshComponent* Mesh1P;
+
+	/** First person camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* FirstPersonCameraComponent;
+
+	/** MappingContext */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	UInputMappingContext* DefaultMappingContext;
+
+	/** Jump Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	UInputAction* JumpAction;
+
+	/** Move Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category=Input, meta=(AllowPrivateAccess = "true"))
+	UInputAction* MoveAction;
+
+public:
+	AOneVOneMeCharacter();
+
+protected:
+	virtual void BeginPlay();
+
+public:
+		
+	/** Look Input Action */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = Input, meta = (AllowPrivateAccess = "true"))
+	class UInputAction* LookAction;
+
+	/** Bool for AnimBP to switch to another animation set */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Weapon)
+	bool bHasRifle;
+
+	/** Health of the character */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "Character", meta = (ClampMin = "0.0"))
+	float Health;
+
+	/** Initial health value */
+	float InitialHealth;
+
+	/** Setter to set the bool */
+	UFUNCTION(BlueprintCallable, Category = Weapon)
+	void SetHasRifle(bool bNewHasRifle);
+
+	/** Getter for the bool */
+	UFUNCTION(BlueprintCallable, Category = Weapon)
+	bool GetHasRifle();
+
+	/** Function to apply damage to the character */
+	UFUNCTION(BlueprintCallable, Category = "Character")
+	void TakeDamage(float Damage);
+
+	/** Function to respawn the character */
+	UFUNCTION(BlueprintCallable, Category = "Character")
+	void Respawn();
+
+protected:
+	/** Called for movement input */
+	void Move(const FInputActionValue& Value);
+
+	/** Called for looking input */
+	void Look(const FInputActionValue& Value);
+
+protected:
+	// APawn interface
+	virtual void SetupPlayerInputComponent(UInputComponent* InputComponent) override;
+	// End of APawn interface
+
+public:
+	/** Returns Mesh1P subobject **/
+	USkeletalMeshComponent* GetMesh1P() const { return Mesh1P; }
+	/** Returns FirstPersonCameraComponent subobject **/
+	UCameraComponent* GetFirstPersonCameraComponent() const { return FirstPersonCameraComponent; }
+
+	/** Called when the character dies */
+	void Die();
+
+	/** Returns whether the character is alive */
+	UFUNCTION(BlueprintPure, Category = "Character")
+	bool IsAlive() const { return Health > 0.0f; }
+
+	/** Returns the current health of the character */
+	UFUNCTION(BlueprintPure, Category = "Character")
+	float GetHealth() const { return Health; }
+
+	/** Returns the maximum health of the character */
+	UFUNCTION(BlueprintPure, Category = "Character")
+	float GetMaxHealth() const { return InitialHealth; }
+};
---
+++ Source/OneVOneMe/OneVOneMeGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "OneVOneMeGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AOneVOneMeGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AOneVOneMeGameMode();
+};
+
+
+
